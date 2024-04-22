d3cfc317bac7c0a5ad71214ecc050f69defdea81
juseojo <j86371224@gmail.com>
2024-04-23 02:08:12 +0900

    get data from server

--- Mullo/ViewModel/Main_viewModel.swift
+++ Mullo/ViewModel/Main_viewModel.swift
@@ -21,49 +21,39 @@ class Main_viewModel
 
 	func get_cell_data()
 	{
-		get_data()
+		items
 			.observe(on: MainScheduler.instance)
 			.subscribe(onNext: { [weak self] results in
 				self?.subject.onNext(results)
 			})
 			.disposed(by: disposeBag)
+		get_data()
 	}
 
-	private func get_data() -> Observable<[Post_cell_data]>  {
-		return Observable<[Post_cell_data]>.create { observer in
-			AF.request(
-				"http://\(host)/get_post?offset=0",
-				method: .get,
-				encoding: URLEncoding.httpBody).responseJSON() { response in
-				switch response.result {
-				case .success:
-					if let data = try! response.result.get() as? [[String]] {
-						var result = [Post_cell_data]()
-
-						for source in data
-						{
-							print(source)
-							let post_cell_data = Post_cell_data()
-
-							post_cell_data.name_text = source[0]
-							post_cell_data.time_text = source[1]
-							post_cell_data.post_text = source[2]
-							post_cell_data.choice_text = source[3]
-
-							result.append(post_cell_data)
-						}
-						print(result)
-						// 실제 데이터 전달
-						self.subject.onNext(result)
-						// 완료 처리
-						self.subject.onCompleted()
+	private func get_data() {
+		AF.request(
+			"http://\(host)/get_post?offset=0",
+			method: .get,
+			encoding: URLEncoding.httpBody).validate().responseJSON() { response in
+			switch response.result {
+			case .success:				//get resource and convert to [[String]]
+				if let data = try! response.result.get() as? [[String]] {
+					//data classify
+					let post_cell_dataSet = data.map { source -> Post_cell_data in
+						return Post_cell_data(
+							name_text: source[0],
+							time_text: source[1],
+							post_text: source[2],
+							choice_text: source[3]
+						)
 					}
-				case .failure(let error):
-					print("Error: \(error)")
-					self.subject.onError(error)
+					self.subject.onNext(post_cell_dataSet)
+					self.subject.onCompleted()
 				}
+			case .failure(let error):
+				print("Error: \(error)")
+				self.subject.onError(error)
 			}
-			return self.subject.asObservable().subscribe()
 		}
 	}
 }
