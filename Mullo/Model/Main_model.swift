40d1d6aabbc0ad2f5399cfcae7cc1ec41b87d04e
juseojo <j86371224@gmail.com>
2024-04-23 02:10:18 +0900

    init

--- Mullo/Model/Main_model.swift
+++ Mullo/Model/Main_model.swift
@@ -10,4 +10,11 @@ class Post_cell_data {
 	var time_text: String = "none"
 	var post_text: String = "none"
 	var choice_text: String = "none"
+
+	init(name_text: String, time_text: String, post_text: String, choice_text: String) {
+		self.name_text = name_text
+		self.time_text = time_text
+		self.post_text = post_text
+		self.choice_text = choice_text
+	}
 }
