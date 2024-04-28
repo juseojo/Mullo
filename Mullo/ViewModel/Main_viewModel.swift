738e351cc724ab30df67b27f6cb9680de1af2aa5
juseojo <j86371224@gmail.com>
2024-04-28 21:46:32 +0900

    code cleaning
    add remove post func for reload

--- Mullo/ViewModel/Main_viewModel.swift
+++ Mullo/ViewModel/Main_viewModel.swift
@@ -13,47 +13,41 @@ import SnapKit
 
 class Main_viewModel
 {
+	private var post_dataSet = [Post_cell_data]()
 	private let disposeBag = DisposeBag()
 	private let subject = BehaviorSubject<[Post_cell_data]>(value: [])
 	var items: Observable<[Post_cell_data]> {
-		return subject.asObservable()
+		return subject.compactMap { $0 }
 	}
 
-	func get_cell_data()
-	{
-		items
-			.observe(on: MainScheduler.instance)
-			.subscribe(onNext: { [weak self] results in
-				self?.subject.onNext(results)
-			})
-			.disposed(by: disposeBag)
-		get_data()
-	}
-
-	private func get_data() {
+	func get_data(index: Int) {
 		AF.request(
-			"http://\(host)/get_post?offset=0",
+			"http://\(host)/get_post?offset=\(index)",
 			method: .get,
 			encoding: URLEncoding.httpBody).validate().responseJSON() { response in
 			switch response.result {
 			case .success:				//get resource and convert to [[String]]
 				if let data = try! response.result.get() as? [[String]] {
 					//data classify
-					let post_cell_dataSet = data.map { source -> Post_cell_data in
+					self.post_dataSet.append(contentsOf: data.map { source -> Post_cell_data in
 						return Post_cell_data(
 							name_text: source[0],
 							time_text: source[1],
 							post_text: source[2],
 							choice_text: source[3]
 						)
-					}
-					self.subject.onNext(post_cell_dataSet)
-					self.subject.onCompleted()
+					})
+					self.subject.onNext(self.post_dataSet)
 				}
 			case .failure(let error):
 				print("Error: \(error)")
 				self.subject.onError(error)
 			}
 		}
+	}
+
+	func remove_all()
+	{
+		post_dataSet.removeAll()
 	}
 }
