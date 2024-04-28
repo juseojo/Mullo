7971be6bde7e8811ca674a22ef9b12fa88fc4412
juseojo <j86371224@gmail.com>
2024-04-28 21:46:51 +0900

    Add substr func

--- Mullo/util.swift
+++ Mullo/util.swift
@@ -11,3 +11,27 @@ let screen_width = UIScreen.main.bounds.size.width
 let screen_height = UIScreen.main.bounds.size.height
 let head_height: CGFloat = screen_height * 0.05
 let top_inset = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
+
+extension String {
+	func substr(seperater: Character) -> [String] {
+		var result = [String]()
+		var text = ""
+		var count = 0
+
+		for char in self
+		{
+			if char == seperater
+			{
+				result.append(text)
+				text = ""
+				count += 1
+				continue
+			}
+
+			text.append(char)
+		}
+
+		result.append(text)
+		return result
+	}
+}
