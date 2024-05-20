d342352194efb6b282fa657c7b59c953f5dfb51a
juseojo <j86371224@gmail.com>
2024-05-20 23:34:18 +0900

    + google login

--- Mullo/AppDelegate.swift
+++ Mullo/AppDelegate.swift
@@ -7,16 +7,24 @@
 
 import UIKit
 
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		FirebaseApp.configure()
+
 		return true
 	}
 
+	func application(_ app: UIApplication, open url: URL,  options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+		//for google login
+	  return GIDSignIn.sharedInstance.handle(url)
+	}
 	// MARK: UISceneSession Lifecycle
 
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
