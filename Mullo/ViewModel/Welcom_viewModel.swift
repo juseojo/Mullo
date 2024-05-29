463917c3254cb4cc2035ffc63d9c1b5decf0b589
juseojo <j86371224@gmail.com>
2024-05-29 16:34:17 +0900

    + getting name - changing view ( move to vc )

--- Mullo/ViewModel/Welcom_viewModel.swift
+++ Mullo/ViewModel/Welcom_viewModel.swift
@@ -53,11 +53,11 @@ final class Welcome_viewModel {
 		}
 	}
 
-	final func register_name(name: String, result_handler: @escaping (String) -> Void)
+	final func register_name(name: String, email: String, result_handler: @escaping (String) -> Void)
 	{
 		AF.request(
-			"http://\(host)/register",
-			method: .post, parameters: ["name" : name],
+			"https://\(host)/register",
+			method: .post, parameters: ["name" : name, "email" : email],
 			encoding: URLEncoding.httpBody)
 				.validate(statusCode: 200..<300)
 				.validate(contentType: ["application/json"])
@@ -67,24 +67,51 @@ final class Welcome_viewModel {
 				do {
 					let data = try response.result.get()
 
-					result_handler(data["result"] ?? "error")
+					result_handler(data["result"] ?? "|error")
 				} catch {
-					print("-- Error at getting current images --\n \(error)")
-					result_handler("error")
+					print("-- Error at register_name --\n \(error)")
+					result_handler("|error")
 				}
 			case .failure(let error):
 				print("Error: \(error)")
-				result_handler("error")
+				result_handler("|error")
 			}
 		}
 	}
 
-	final func google_login(vc: Welcome_viewController)
+	final func get_name(email: String, result_handler: @escaping (String) -> Void)
+	{
+		AF.request(
+			"https://\(host)/get_name",
+			method: .post, parameters: ["email" : email],
+			encoding: URLEncoding.httpBody)
+				.validate(statusCode: 200..<300)
+				.validate(contentType: ["application/json"])
+				.responseDecodable(of: [String:String].self) { response in
+			switch response.result {
+			case .success:
+				do {
+					let data = try response.result.get()
+
+					result_handler(data["result"] ?? "|error")
+				} catch {
+					print("-- Error at get_name --\n \(error)")
+					result_handler("|error")
+				}
+			case .failure(let error):
+				print("Error: \(error)")
+				result_handler("|error")
+			}
+		}
+	}
+
+	final func google_login(vc: Welcome_viewController, result_handler: @escaping (Bool) -> Void)
 	{
 		guard let clientID = FirebaseApp.app()?.options.clientID 
 		else 
 		{
 			print("Firebase clientID not found.")
+			result_handler(false)
 
 			return
 		}
@@ -98,11 +125,15 @@ final class Welcome_viewModel {
 
 			guard error == nil, let result = result else {
 				print("---Google Sign-In fail---\n\(error?.localizedDescription ?? "Unknown error")")
+				result_handler(false)
+
 				return
 			}
 
 			guard let idToken = result.user.idToken?.tokenString else {
 				print("---Google Sign-In token retrieval fail---")
+				result_handler(false)
+
 				return
 			}
 
@@ -110,55 +141,19 @@ final class Welcome_viewModel {
 
 			let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
 
+			// Do google login
 			Auth.auth().signIn(with: credential) { result, error in
 				// At this point, our user is signed in
 				if let error = error {
 					print("---Google Sign-In authentication fail---\n\(error.localizedDescription)")
+					result_handler(false)
+
 					return
 				}
 
-				print("signin success")
-
-				// Case of didn't set name
-				if UserDefaults.standard.string(forKey: "name") == nil
-				{
-					// Change view ( current view -> name view )
-					vc.welcome_view.register_view.removeFromSuperview()
-					vc.welcome_view.login_view.removeFromSuperview()
-
-					vc.welcome_view.name_view.snp.makeConstraints { make in
-						make.top.equalTo(vc.welcome_view.welcome_label.snp.bottom).offset(50)
-						make.left.right.equalTo(vc.welcome_view)
-						make.height.equalTo(100)
-					}
-					
-					if vc.welcome_view.login_button.superview != nil
-					{
-						vc.welcome_view.login_button.snp.remakeConstraints { make in
-							make.top.equalTo(vc.welcome_view.name_view.snp.bottom)
-							make.left.right.equalTo(vc.welcome_view).inset(30)
-							make.height.equalTo(55)
-						}
-					}
-					else
-					{
-						vc.welcome_view.register_button.snp.remakeConstraints { make in
-							make.top.equalTo(vc.welcome_view.name_view.snp.bottom)
-							make.left.right.equalTo(vc.welcome_view).inset(30)
-							make.height.equalTo(55)
-						}
-					}
-				}
-				else
-				{
-					// Change VC ( current VC -> Main VC )
-					let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
-					guard let delegate = sceneDelegate else {
-						print("sceneDelegate delegate error")
-						return
-					}
-					delegate.changeRootVC(Main_ViewController(), animated: true)
-				}
+				print("google signin success")
+				vc.email_address = result?.user.email ?? ""
+				result_handler(true)
 			}
 		}
 	}
