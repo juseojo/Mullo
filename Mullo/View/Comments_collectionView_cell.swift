666b574e22546832f69f992f02caaa23aa9a3b51
juseojo <j86371224@gmail.com>
2024-07-31 19:59:49 +0900

    + layout, design + up button's selected type + prepareForReuse

--- Mullo/View/Comments_collectionView_cell.swift
+++ Mullo/View/Comments_collectionView_cell.swift
@@ -11,6 +11,8 @@ class Comments_collectionView_cell: UICollectionViewCell
 {
 	static let identifier = "comments"
 
+	var comment_num: Int = -1
+
 	var name_label: UILabel = {
 		let name_label = UILabel()
 
@@ -32,7 +34,9 @@ class Comments_collectionView_cell: UICollectionViewCell
 	var report_button: UIButton = {
 		let report_button = UIButton()
 
-		report_button.setImage(UIImage(systemName: "flag"), for: .normal)
+		report_button.setImage(
+			UIImage(systemName: "flag", withConfiguration: UIImage.SymbolConfiguration(pointSize: 25, weight: .bold, scale: .large)),
+			for: .normal)
 		report_button.tintColor = UIColor(named: "REVERSE_SYS")
 
 		return report_button
@@ -43,15 +47,24 @@ class Comments_collectionView_cell: UICollectionViewCell
 
 		comment_label.font = UIFont(name: "SeoulHangangM", size: 15)
 		comment_label.tintColor = UIColor(named: "REVERSE_SYS")
-
+		comment_label.numberOfLines = 0
+		
 		return comment_label
 	}()
 
 	var up_button: UIButton = {
 		let up_button = UIButton()
 
-		up_button.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+		up_button.setImage(
+			UIImage(systemName: "hand.thumbsup", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .bold, scale: .large)),
+			for: .normal)
+		up_button.setImage(
+			UIImage(systemName: "hand.thumbsup.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .bold, scale: .large)),
+			for: .selected)
 		up_button.tintColor = UIColor(named: "REVERSE_SYS")
+		up_button.setTitleColor(UIColor(named: "REVERSE_SYS"), for: .normal)
+		up_button.contentHorizontalAlignment = .left
+		up_button.imageView?.contentMode = .scaleAspectFit
 
 		return up_button
 	}()
@@ -89,7 +102,7 @@ class Comments_collectionView_cell: UICollectionViewCell
 		}
 
 		report_button.snp.makeConstraints { make in
-			make.top.right.equalTo(self)
+			make.bottom.right.equalTo(self).inset(5)
 			make.width.height.equalTo(25)
 		}
 
@@ -102,16 +115,26 @@ class Comments_collectionView_cell: UICollectionViewCell
 
 		up_button.snp.makeConstraints { make in
 			make.top.equalTo(comment_label.snp.bottom).inset(-10)
-			make.left.equalTo(self)
-			make.bottom.equalTo(border_view.snp.top)
-			make.width.equalTo(50)
+			make.left.equalTo(self).inset(5)
+			make.height.equalTo(25)
+			make.width.equalTo(100)
 		}
 
 		border_view.snp.makeConstraints { make in
-			make.left.bottom.equalTo(self)
-			make.width.equalTo(screen_width)
+			make.top.equalTo(up_button.snp.bottom).inset(-10)
+			make.left.right.equalTo(self)
 			make.height.equalTo(1)
 		}
+	}
+
+	override func prepareForReuse() {
+
+		time_label.text = nil
+		name_label.text = nil
+		comment_label.text = nil
+		up_button.isSelected = false
+		up_button.titleLabel?.text = nil
+		comment_num = -1
 	}
 
 	final func nameLabel_width_setting(width: Int)
