896a04ae3870ef8c2faa6e440a11a831e8b9a11e
juseojo <j86371224@gmail.com>
2024-07-31 19:53:23 +0900

    + collect rx bindings + dynamic height + comment add button event + popularSort_button event + recentSort_button evet

--- Mullo/ViewController/Comments_viewController.swift
+++ Mullo/ViewController/Comments_viewController.swift
@@ -27,23 +27,7 @@ final class Comments_viewController: UIViewController
 		comments_view.comments_collectionView.delegate = self
 
 		// rx binding
-		comments_view.close_button.rx.tap
-			.bind {
-				self.dismiss(animated: true)
-			}.disposed(by: disposeBag)
-		
-		bind_collectionView()
-
-		comments_view.comment_add_button.rx.tap
-			.bind {
-				let parameters: [String:String] = [
-					"post_num" : String(self.post_num),
-					"name" : UserDefaults.standard.string(forKey: "name") ?? "none",
-					"time" : get_time_now(),
-					"comment" : self.comments_view.comment_textView.text
-				]
-				self.comments_viewModel.add_comment(parameters: parameters)
-			}.disposed(by: disposeBag)
+		rx_binding()
 
 		// Get cell's dynamic height
 		comments_viewModel.items
@@ -74,39 +58,97 @@ final class Comments_viewController: UIViewController
 		completion_handler!()
 	}
 
-	private func save_cell_height(item: Comments_cell_data)
+	final func rx_binding()
+	{
+		// close_button binding
+		comments_view.close_button.rx.tap
+			.bind { [weak self] in
+				self!.dismiss(animated: true)
+			}.disposed(by: disposeBag)
+
+		// comments_collectionView binding
+		comments_viewModel.items
+			.observe(on: MainScheduler.instance)
+			.bind( to: comments_view.comments_collectionView.rx.items(
+				cellIdentifier: Comments_collectionView_cell.identifier,
+				cellType: Comments_collectionView_cell.self)) { [weak self] row, item, cell in
+					// cell setting
+					self!.comments_viewModel.cell_setting(cell: cell, item: item)
+				}.disposed(by: self.disposeBag)
+
+		// comment_add_button binding
+		comments_view.comment_add_button.rx.tap
+			.bind { [weak self] in
+				let parameters: [String:String] = [
+					"post_num" : String(self!.post_num),
+					"name" : UserDefaults.standard.string(forKey: "name") ?? "none",
+					"time" : get_time_now(),
+					"comment" : self!.comments_view.comment_textView.text
+				]
+				self!.comments_viewModel.add_comment(parameters: parameters)
+				self!.comments_viewModel.remove_all()
+
+				DispatchQueue.main.asyncAfter(deadline: .now() + 0.2)
+				{
+					self!.comments_viewModel.get_comments(
+						index: 0, post_num: self!.post_num, isSortByPopular: false) { _ in
+							// Have to add error control
+						}
+				}
+				self!.comments_view.comment_textView.text = ""
+				self!.view.endEditing(true)
+
+			}.disposed(by: disposeBag)
+
+		// popularSort_button binding
+		comments_view.popularSort_button.rx.tap
+			.bind { [weak self] in
+				self!.comments_viewModel.remove_all()
+				self!.comments_viewModel.get_comments(index: 0, post_num: self!.post_num, isSortByPopular: true) { isSuccess in
+					if isSuccess == false {
+						// Have to add error control
+					}
+				}
+			}.disposed(by: disposeBag)
+
+		// recentSort_button binding
+		comments_view.recentSort_button.rx.tap
+			.bind { [weak self] in
+				self!.comments_viewModel.remove_all()
+				self!.comments_viewModel.get_comments(index: 0, post_num: self!.post_num, isSortByPopular: false) { isSuccess in
+					if isSuccess == false {
+						// Have to add error control
+					}
+				}
+			}.disposed(by: disposeBag)
+
+	}
+
+	final func save_cell_height(item: Comments_cell_data)
 	{
 		let height = calculate_height(
-			text: item.comment, font: UIFont(name: "SeoulHangangM", size: 15)!, width: screen_width - 20) + 30
+			text: item.comment, font: UIFont(name: "SeoulHangangM", size: 15)!, width: screen_width - 20) + 95
 
 		cell_height_array.append(height)
 	}
 
-	func bind_collectionView()
+	final func bind_collectionView()
 	{
-		comments_viewModel.items
-			.observe(on: MainScheduler.instance)
-			.bind(to: comments_view.comments_collectionView.rx.items(
-				cellIdentifier: Comments_collectionView_cell.identifier, cellType: Comments_collectionView_cell.self)) { row, item, cell in
-					// cell setting
-					self.comments_viewModel.cell_setting(cell: cell, item: item)
-				}.disposed(by: self.disposeBag)
+
 	}
 }
 
 extension Comments_viewController: UICollectionViewDelegateFlowLayout {
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
-
 		//for dynamic height
 		if cell_height_array.count == 0 || cell_height_array.count <= indexPath.row
 		{
 			print("cell_height_array not ready")
-			return CGSize(width: screen_width, height: 400)
+			return CGSize(width: screen_width, height: 100)
 		}
-		 
 
-		return CGSize(width: screen_width, height: 100)
+		return CGSize(width: screen_width, height: cell_height_array[indexPath.row])
 	}
 
 	//for infinity scroll
