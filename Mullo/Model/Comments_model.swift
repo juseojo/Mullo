e6dd07071e57d938bcabe42fed077c7bf63942c4
juseojo <j86371224@gmail.com>
2024-07-31 19:50:46 +0900

    + comment_num + save choose comments's num by realm

--- Mullo/Model/Comments_model.swift
+++ Mullo/Model/Comments_model.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 struct Comments_cell_data: Codable{
 	var post_num: Int
@@ -13,12 +14,27 @@ struct Comments_cell_data: Codable{
 	var time: String
 	var comment: String
 	var up_count: Int
+	var comment_num: Int
 
-	init(post_num: Int, name: String, time: String, comment: String, up_count: Int) {
+	init(post_num: Int, name: String, time: String, comment: String, up_count: Int, comment_num: Int) {
 		self.post_num = post_num
 		self.name = name
 		self.time = time
 		self.comment = comment
 		self.up_count = up_count
+		self.comment_num = comment_num
 	}
+}
+
+class Choose_upButton: Object {
+	@Persisted var comment_num: Int
+
+	convenience init(comment_num: Int) {
+		self.init()
+		self.comment_num = comment_num
+	}
+}
+
+class Comment_DB: Object {
+	@Persisted var choose_upButtons: List<Choose_upButton>
 }
