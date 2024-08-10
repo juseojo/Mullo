8e8a03d388cf026d0c8f12be813ac34992790bb3
juseojo <j86371224@gmail.com>
2024-08-10 22:40:36 +0900

    + setting view UI

--- Mullo/View/Setting_view.swift
+++ Mullo/View/Setting_view.swift
@@ -6,11 +6,145 @@
 //
 
 import UIKit
+import SnapKit
 
 class Setting_view: UIView {
 
+	var close_button: UIButton = {
+		let close_button = UIButton()
+
+		close_button.setImage(
+			UIImage(systemName: "xmark")?.resized(to: CGSize(width: 30, height: 30)), for: .normal)
+		close_button.tintColor = UIColor(named: "REVERSE_SYS")
+		close_button.contentMode = .scaleAspectFit
+		
+		return close_button
+	}()
+
+	var setting_label: UILabel = {
+		let setting_label = UILabel()
+
+		setting_label.text = "설정"
+		setting_label.tintColor = UIColor(named: "REVERSE_SYS")
+		setting_label.font = UIFont(name: "Urbanist-Bold", size: 20)
+
+		return setting_label
+	}()
+
+	var border_view: UIView = {
+		let border_view = UIView()
+
+		border_view.backgroundColor = UIColor.gray
+
+		return border_view
+	}()
+
+	var question_button: UIButton = {
+		let question_button = UIButton()
+
+		question_button.layer.borderColor = UIColor(named: "REVERSE_SYS")!.cgColor
+		question_button.layer.borderWidth = 1.0
+		question_button.setTitle("문의하기", for: .normal)
+		question_button.titleLabel?.font = UIFont(name: "Urbanist-SemiBold", size: 20)
+		question_button.setTitleColor(UIColor(named: "REVERSE_SYS"), for: .normal)
+		question_button.layer.cornerRadius = 5.0
+
+		return question_button
+	}()
+
+	var right_button: UIButton = {
+		let right_button = UIButton()
+
+		right_button.layer.borderColor = UIColor(named: "REVERSE_SYS")!.cgColor
+		right_button.layer.borderWidth = 1.0
+		right_button.setTitle("권리표기", for: .normal)
+		right_button.titleLabel?.font = UIFont(name: "Urbanist-SemiBold", size: 20)
+		right_button.setTitleColor(UIColor(named: "REVERSE_SYS"), for: .normal)
+		right_button.layer.cornerRadius = 5.0
+
+		return right_button
+	}()
+
+	var logout_button: UIButton = {
+		let logout_button = UIButton()
+
+		logout_button.layer.borderColor = UIColor.red.cgColor
+		logout_button.layer.borderWidth = 1.0
+		logout_button.setTitle("로그아웃", for: .normal)
+		logout_button.titleLabel?.font = UIFont(name: "Urbanist-SemiBold", size: 20)
+		logout_button.setTitleColor(.red, for: .normal)
+		logout_button.layer.cornerRadius = 5.0
+
+		return logout_button
+	}()
+
+	var deleteID_button: UIButton = {
+		let deleteID_button = UIButton()
+
+		deleteID_button.layer.borderColor = UIColor.red.cgColor
+		deleteID_button.layer.borderWidth = 1.0
+		deleteID_button.setTitle("탈퇴하기", for: .normal)
+		deleteID_button.titleLabel?.font = UIFont(name: "Urbanist-SemiBold", size: 20)
+		deleteID_button.setTitleColor(.red, for: .normal)
+		deleteID_button.layer.cornerRadius = 5.0
+
+		return deleteID_button
+	}()
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+
+		self.backgroundColor = UIColor(named: "NATURAL")
+
+		addSubview(close_button)
+		addSubview(setting_label)
+		addSubview(border_view)
+		addSubview(question_button)
+		addSubview(right_button)
+		addSubview(logout_button)
+		addSubview(deleteID_button)
+
+		close_button.snp.makeConstraints { make in
+			make.top.left.equalTo(self).inset(10)
+			make.width.height.equalTo(30)
+		}
+
+		setting_label.snp.makeConstraints { make in
+			make.top.equalTo(self).inset(10)
+			make.left.equalTo(close_button.snp.right).inset(-10)
+			make.right.equalTo(self)
+			make.height.equalTo(30)
+		}
+
+		border_view.snp.makeConstraints { make in
+			make.top.equalTo(close_button.snp.bottom).inset(-10)
+			make.left.right.equalTo(self)
+			make.height.equalTo(1)
+		}
+
+		question_button.snp.makeConstraints { make in
+			make.top.equalTo(border_view.snp.bottom).inset(-20)
+			make.left.right.equalTo(self).inset(20)
+			make.height.equalTo(60)
+		}
+
+		right_button.snp.makeConstraints { make in
+			make.top.equalTo(question_button.snp.bottom).inset(-20)
+			make.left.right.equalTo(self).inset(20)
+			make.height.equalTo(60)
+		}
+
+		logout_button.snp.makeConstraints { make in
+			make.top.equalTo(right_button.snp.bottom).inset(-20)
+			make.left.right.equalTo(self).inset(20)
+			make.height.equalTo(60)
+		}
+
+		deleteID_button.snp.makeConstraints { make in
+			make.top.equalTo(logout_button.snp.bottom).inset(-20)
+			make.left.right.equalTo(self).inset(20)
+			make.height.equalTo(60)
+		}
 	}
 	
 	required init?(coder: NSCoder) {
